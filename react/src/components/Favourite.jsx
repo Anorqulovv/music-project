f90491024fa4484c6902d49e550ffefcc34810b3
Anorqulov14 <eldoranorqulov@gmail.com>
2025-12-10 14:28:35 +0500

    first branch

---
+++ react/src/components/Favourite.jsx
@@ -0,0 +1,8 @@
+
+export default function Favourite() {
+  return (
+    <div>
+      <h1>hello Favourite</h1>
+    </div>
+  )
+}
---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { NavLink, Outlet} from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <div>
+      <NavLink to='/all'>All</NavLink>
+    </div>
+  )
+}
